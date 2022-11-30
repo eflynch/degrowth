79cba2c45c64512c5086a52509db5188885cb5d7
Evan Lynch <evan.f.lynch@gmail.com>
2022-11-29 20:29:38 -0500

    Chec prettier

--- degrowth/src/components/pages/First.tsx
+++ degrowth/src/components/pages/First.tsx
@@ -2,7 +2,9 @@ import { ReactElement } from "react";
 import useAction from "../../hooks/useAction";
 
 export default function First():ReactElement {
-    const goToLanding = useAction(["GoToPage", {page:"Landing"}])
+    const goToLanding = useAction([
+        "GoToPage",
+                        {page:"Landing"}])
     return (
         <>
             First
